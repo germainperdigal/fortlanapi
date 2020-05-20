7b8aca343aa9c628b46d2e3b3bb00437551a523a
Germain PERDIGAL <perdigalgermain@gmail.com>
2020-05-20 09:59:23 +0200

    NEW PORT

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const app = require('./app');
 const https = require('https');
 var fs = require('fs');
 
-// var privateKey = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/privkey.pem');
-// var certificate = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/cert.pem');
-// var ca = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/chain.pem');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/privkey.pem');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/cert.pem');
+var ca = fs.readFileSync('/etc/letsencrypt/live/fifbet.com/chain.pem');
 
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca
-// }
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+}
 
-const port = process.env.PORT || 6544;
+const port = process.env.PORT || 9494;
 const server = http.createServer(app);
 
-// const httpsServer = https.createServer(credentials, app);
-// httpsServer.listen(9865, () => {
-//     console.log("HTTPS FORTLAN RUNNING 9865")
-// })
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(1122, () => {
+    console.log("HTTPS FORTLAN RUNNING 1122")
+})
 
 
 server.listen(port, function() {
-    console.log("HTTPS FORTLAN RUNNING 6544");
+    console.log("HTTPS FORTLAN RUNNING 9494");
 });
